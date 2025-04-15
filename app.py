e3dfab1e1b6e59f29a21ecec04f72912cab25665
Khwahish29 <khwahish.p1@ahduni.edu.in>
2025-04-15 22:53:34 +0530

    update system prompt

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ query = st.chat_input("Say something: ")
 prompt = query
 
 system_prompt = (
-    "You are an assistant for question-answering tasks. "
+    "You are an expert in medicines, assisting for "
+    "question-answering tasks related to drug data. "
     "Use the following pieces of retrieved context to answer "
     "the question. If you don't know the answer, say that you "
     "don't know. Use three sentences maximum and keep the "
